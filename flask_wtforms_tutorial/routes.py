19f6a9d18fa4278b933f37f34262f22a6cf4da78
swimmingnoah <noah@nemecfamily.com>
2021-05-03 13:15:23 -0500

    first commit

---
+++ flask_wtforms_tutorial/routes.py
@@ -0,0 +1,35 @@
+from flask import current_app as app
+from flask import redirect, render_template, url_for, request, flash
+
+from .forms import *
+
+
+#@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
+def user_options():
+    
+    form = UserOptionForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        option = request.form['option']
+
+        if option == "1":
+            return redirect('/admin')
+        else:
+            return redirect("/reservations")
+    
+    return render_template("options.html", form=form, template="form-template")
+
+@app.route("/admin", methods=['GET', 'POST'])
+def admin():
+
+    form = AdminLoginForm()
+
+    return render_template("admin.html", form=form, template="form-template")
+
+@app.route("/reservations", methods=['GET', 'POST'])
+def reservations():
+
+    form = ReservationForm()
+
+    return render_template("reservations.html", form=form, template="form-template")
+
