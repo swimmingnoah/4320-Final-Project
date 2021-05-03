774288a846907f29de01a0e37bbaa22888c6aff2
Kate Clemens <46911759+klchbm@users.noreply.github.com>
2021-05-03 14:23:06 -0500

    Updated kate.py

--- flask_wtforms_tutorial/kate.py
+++ flask_wtforms_tutorial/kate.py
@@ -1,9 +1,11 @@
 #Stack overflow resource
 
-string_one = "AAA"
-string_two = "INFOTC4320"
+string_name = "Alice"
+string_code = "INFOTC4320"
+string_row = "0"
+string_col = "1"
 
-res = "".join([string_one[i] + string_two[i] for i in range(len(string_one))]) + string_two[len(string_one):]
+res = "".join([string_name[i] + string_code[i] for i in range(len(string_name))]) + string_code[len(string_name):]
 
 # print result
 print("The reservation code : " + str(res))
