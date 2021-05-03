5aed6ef6a358749e8287421e0442dc6aa983321c
Livvy Beggs <obbrbt@mail.missouri.edu>
2021-05-03 13:25:01 -0500

    Added Livvy and Li's code

---
+++ flask_wtforms_tutorial/seatingChart.py
@@ -0,0 +1,26 @@
+# Livvy and Li's code
+def seatchart():
+    chart = [['O','O','O','O'],
+             ['O','O','O','O'],
+             ['O','O','O','O'],
+             ['O','O','O','O'],
+             ['O','O','O','O'],
+             ['O','O','O','O'],
+             ['O','O','O','O'],
+             ['O','O','O','O'],
+             ['O','O','O','O'],
+             ['O','O','O','O'],
+             ['O','O','O','O'],
+             ['O','O','O','O']]
+
+    try:
+        file = open('reservations.txt','r')
+    except FileNotFoundError:
+        err = "ERROR: Can't find file."
+    else:
+         reservations = file.readlines()
+    for  reservation in reservations:
+        row = int(reservation.split(', ')[1])
+        seat = int(reservation.split(', ')[2])
+        chart[row][seat] = 'X'
+    return chart
