29fff644a2f17186958e3fe34d1b05edc4c193fd
Kate Clemens <46911759+klchbm@users.noreply.github.com>
2021-05-03 14:38:57 -0500

    Updated kate.py

--- flask_wtforms_tutorial/kate.py
+++ flask_wtforms_tutorial/kate.py
@@ -1,3 +1,4 @@
+
 #Stack overflow resource
 
 string_name = "Alice"
@@ -8,5 +9,5 @@ string_col = "1"
 res = "".join([string_name[i] + string_code[i] for i in range(len(string_name))]) + string_code[len(string_name):]
 
 # print result
-print("Congratulations " + str(string_name))
+print("Congratulations " + str(string_name) + " row " + str(string_row) + " seat " + str(string_col) + " is now reserved!")
 print("The reservation code : " + str(res))
