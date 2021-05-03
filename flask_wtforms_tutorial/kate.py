1168122ea22e673ff8bba36ae128e6c1e315c57e
Kate Clemens <46911759+klchbm@users.noreply.github.com>
2021-05-03 15:18:28 -0500

    Updated kate.py

--- flask_wtforms_tutorial/kate.py
+++ flask_wtforms_tutorial/kate.py
@@ -1,11 +1,9 @@
 #Alternate string name and code INFOTC4320
-string_name = "Alice"
-string_code = "INFOTC4320"
-string_row = "0"
-string_col = "1"
 
-res = "".join([string_name[i] + string_code[i] for i in range(len(string_name))]) + string_code[len(string_name):]
+def eticket(string_name, string_code, string_row, string_col):
+    res = "".join([string_name[i] + string_code[i] for i in range(len(string_name))]) + string_code[len(string_name):]
 
-#Print reservation row, seat, and ticket number
-print("Congratulations " + str(string_name) + "!" + " Row:" + str(string_row) + " Seat:" + str(string_col) + " is now reserved! Enjoy the trip!")
-print("Your e-ticket number is: " + str(res))
+    #Print reservation row, seat, and ticket number
+    return "Congratulations " + str(string_name) + "!" + " Row:" + str(string_row) + " Seat:" + str(string_col) + " is now reserved! Enjoy the trip!" + "Your e-ticket number is: " + str(res))
+
+print(eticket(string_name, string_code, string_row, string_col))
