ecf6ad52b54651ef1043d128511c05b1dc2d229d
Kate Clemens <46911759+klchbm@users.noreply.github.com>
2021-05-03 14:05:40 -0500

    Updated kate.py file

--- flask_wtforms_tutorial/kate.py
+++ flask_wtforms_tutorial/kate.py
@@ -1,28 +1,11 @@
+#Stack overflow resource
+string_one = "x"
+string_two = "INFOTC4320"
 
-
-# try:
-#         file = open('reservations.txt','r')
-#     except FileNotFoundError:
-#         err = "ERROR: Can't find file."
-#     else:
-#          reservations = file.readlines()
-
-
-# def reservationCode(name):
-#
-#     code = 'INFOTC4320'
-#     reservation = alternate(name, code)
-#     finalReservationCode(first_name, row, seat, reservation)
-#     return final
-
-# initializing list
-test_list = ["abc", "xyz"]
-
-
-# using list comprehension + list slicing
-# Alternate Strings Concatenation
-res = [" ".join(test_list[i : : 2]) for i in range(len(test_list) //
-                                                  (len(test_list)//2))]
+def alt(s, t):
+    if not (s and t):
+        return s + t
+    return s[0] + t[0] + alt(s[1:], t[1:])
 
 # print result
-print("The reservation code : " + str(res))
+print("The reservation code : " + str())
